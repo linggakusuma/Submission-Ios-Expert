c879572778f9ba242ef07ac0bc4fe758a0ed1a0f
linggakusumasakti <linggakusumasakti@gmail.com>
2021-09-12 09:51:16 +0700

    Initial Commit

---
+++ Submission Ios Expert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Submission Ios Expert
+//
+//  Created by Lingga Kusuma Sakti on 12/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
